c6ff171fba7b24003cf573215674c3cca19c9ba6
yuhao <yuhao007007007@163.com>
2015-01-09 00:41:30 +0800

    2

---
+++ js/public.js
@@ -0,0 +1,16 @@
+$(function(){
+	/*$('#header .nav li').hover(function(){
+		var that=$(this);
+		that.find('.nav-ch').stop(true,true).fadeOut('fast',function(){
+			that.find('.nav-e').stop(true,true).fadeIn('fast');
+		});
+	},function(){
+		that.find('.nav-e').stop(true,true).fadeOut('fast',function(){
+			that.find('.nav-ch').stop(true,true).fadeIn('fast');
+		});
+	});*/
+	
+	
+	
+	
+});
