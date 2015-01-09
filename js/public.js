443ce22279fa79258e5067b88193542ebcdb89c1
joybee007 <yuhao007007007@163.com>
2015-01-09 11:11:50 +0800

    2

--- js/public.js
+++ js/public.js
@@ -1,16 +1,7 @@
 $(function(){
-	/*$('#header .nav li').hover(function(){
-		var that=$(this);
-		that.find('.nav-ch').stop(true,true).fadeOut('fast',function(){
-			that.find('.nav-e').stop(true,true).fadeIn('fast');
-		});
-	},function(){
-		that.find('.nav-e').stop(true,true).fadeOut('fast',function(){
-			that.find('.nav-ch').stop(true,true).fadeIn('fast');
-		});
-	});*/
-	
-	
-	
-	
+
+
+
+
+
 });
