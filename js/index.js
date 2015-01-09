92d6ec3935869a696e9352b58aee8fb5afc5152b
yuhao <yuhao007007007@163.com>
2015-01-09 23:17:32 +0800

    alpha

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $(function(){
 	$('#marqueebox').y_marquee({
 		nav:'#marqueeNav',
 		prev:'.marquee-left',
-		next:'marquee-right',
+		next:'.marquee-right',
 		onChange:function(i,ci){
 			marqueeTextList.eq(ci).find('.marquee-text-index-short').animate({marginLeft:830},'600','easeInBack').end().find('.marquee-text-index-long').delay(200).animate({marginLeft:830},'600','easeInBack',function(){
 				marqueeTextList.eq(i).show().find('.marquee-text-index-short').animate({marginLeft:0},'600','easeOutBack').end().find('.marquee-text-index-long').delay(200).animate({marginLeft:0},'600','easeOutBack',function(){
